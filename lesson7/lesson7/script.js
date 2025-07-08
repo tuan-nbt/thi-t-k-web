4612824d4a921a6a5f8534afb65425fa6a2e8bc8
tuan-nbt <nguyenbatuanth36@gmail.com>
2025-07-08 18:33:56 +0700

    Add files via upload

---
+++ lesson7/lesson7/script.js
@@ -0,0 +1,11 @@
+// This file is currently empty as the provided image
+// does not show any dynamic or interactive elements that
+// would require JavaScript.
+
+// If future functionalities like:
+// - Mobile menu toggles
+// - Dropdown navigation
+// - Form validations
+// - Image carousels
+// - AJAX content loading
+// ...were to be added, this is where the JavaScript code would go.
